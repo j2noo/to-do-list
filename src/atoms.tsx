5e7f89400c4f3d8254aeb8b865a47455fd6bdbb3
j2noo <wlsdndml213@naver.com>
2023-07-21 15:15:26 +0900

    [FEAT] recoil selector 사용

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,7 +1,18 @@
-import { atom, useRecoilValue, useRecoilState, useSetRecoilState } from "recoil";
+import { atom, selector } from "recoil";
 export interface IToDo {
   id: number;
   text: string;
   category: "DONE" | "DOING" | "TO_DO";
 }
 export const toDoState = atom<IToDo[]>({ key: "toDo", default: [] });
+export const todoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
+  },
+});
--- src/component/ToDoList.tsx
+++ src/component/ToDoList.tsx
@@ -3,18 +3,32 @@ import { useForm } from "react-hook-form";
 import ToDo from "./ToDo";
 import CreateToDo from "./CreateToDo";
 import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { toDoState, todoSelector } from "../atoms";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const [toDos, doing, done] = useRecoilValue(todoSelector);
   return (
     <div>
       <h1>To Dos</h1>
       <hr></hr>
       <CreateToDo></CreateToDo>
+      <h2>To Do</h2>
       <ul>
         {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo}></ToDo>
+        ))}
+      </ul>{" "}
+      <hr></hr>
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo}></ToDo>
+        ))}
+      </ul>{" "}
+      <hr></hr>
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo}></ToDo>
         ))}
       </ul>
